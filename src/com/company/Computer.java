8e8fbca19e9b0d92bba849cbe5181ac5c2051d5c
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-04-23 21:25:31 +0200

    Computer builder class ready

--- src/com/company/Computer.java
+++ src/com/company/Computer.java
@@ -4,61 +4,60 @@ public class Computer {
     //required parameters
     private String HDD;
     private String RAM;
-    //optional parameters
-    private boolean isGraphichCardEnabled;
-    private boolean isBluetoothEnabled;
 
-    private Computer(ComputerBuilder builder){
-        this.HDD = builder.HDD;
-        this.RAM = builder.RAM;
-        this.isGraphichCardEnabled = builder.isGraphichCardEnabled;
-        this.isBluetoothEnabled = bulder.isBluetoothEnabled;
-    }
+    //optional parameters
+    private boolean isGraphicsCardEnabled;
+    private boolean isBluetoothEnabled;
 
     public String getHDD() {
         return HDD;
-    }
-
-    public void setHDD(String HDD) {
-        this.HDD = HDD;
     }
 
     public String getRAM() {
         return RAM;
     }
 
-    public void setRAM(String RAM) {
-        this.RAM = RAM;
-    }
-
-    public boolean isGraphichCardEnabled() {
-        return isGraphichCardEnabled;
-    }
-
-    public void setGraphichCardEnabled(boolean graphichCardEnabled) {
-        isGraphichCardEnabled = graphichCardEnabled;
+    public boolean isGraphicsCardEnabled() {
+        return isGraphicsCardEnabled;
     }
 
     public boolean isBluetoothEnabled() {
         return isBluetoothEnabled;
     }
 
-    public void setBluetoothEnabled(boolean bluetoothEnabled) {
-        isBluetoothEnabled = bluetoothEnabled;
+    private Computer(ComputerBuilder builder){
+        this.HDD = builder.HDD;
+        this.RAM = builder.RAM;
+        this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
+        this.isBluetoothEnabled = builder.isBluetoothEnabled;
     }
 
+    //Builder nested class
     public static class ComputerBuilder{
-        //required parameters
+        // required parameters
         private String HDD;
         private String RAM;
-        //optional parameters
-        private boolean isGraphicsCardEnabled;
-        private boolean isBlueToothEnabled;
 
-        public ComputerBuilder(String HDD, String RAM) {
-            this.HDD = HDD;
-            this.RAM = RAM;
+        // optional parameters
+        private boolean isGraphicsCardEnabled;
+        private boolean isBluetoothEnabled;
+
+        public ComputerBuilder(String hdd,String ram){
+            this.HDD = hdd;
+            this.RAM = ram;
         }
-        public ComputerBuilder setGraphicsCardEnabled(boolean )
+
+        public ComputerBuilder setGraphicsCardEnabled(boolean graphicsCardEnabled) {
+            this.isGraphicsCardEnabled = graphicsCardEnabled;
+            return this;
+        }
+
+        public ComputerBuilder setBluetoothEnabled(boolean bluetoothEnabled) {
+            this.isBluetoothEnabled = bluetoothEnabled;
+            return this;
+        }
+        public Computer build(){
+            return new Computer(this);
+        }
     }
 }
